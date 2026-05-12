538e38e3be771989fd058b0dd4af61ccdc57571f
Harsh Kumar <tumharaemail@gmail.com>
2026-05-12 16:43:02 +0530

    fixed pages import

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,26 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import VendorManagement from "../pages/admin/VendorManagement";
-import Products from "../pages/admin/Products";
-import PurchaseOrders from "../pages/admin/PurchaseOrders";
-import Invoices from "../pages/admin/Invoices";
-import Payments from "../pages/admin/Payments";
+import VendorManagement from "../Pages/admin/VendorManagement";
+import Products from "../Pages/admin/Products";
+import PurchaseOrders from "../Pages/admin/PurchaseOrders";
+import Invoices from "../Pages/admin/Invoices";
+import Payments from "../Pages/admin/Payments";
+
+import VendorDashboard from "../Pages/vendor/VendorDashboard";
+import VendorSignup from "../Pages/vendor/VendorSignup";
+import VendorPO from "../Pages/vendor/VendorPO";
+import VendorInvoices from "../Pages/vendor/VendorInvoices";
+import Shipment from "../Pages/vendor/Shipment";
+import VendorPayments from "../Pages/vendor/VendorPayments";
+
+import AdminDashboard from "../Pages/admin/AdminDashboard";
+
+import Login from "../Pages/Login";
+
+import VendorApproval from "../Pages/admin/VendorApproval";
+
+// import VendorManagement from "../pages/admin/VendorManagement";
+// import Products from "../pages/admin/Products";
+// import PurchaseOrders from "../pages/admin/PurchaseOrders";
+// import Invoices from "../pages/admin/Invoices";
+// import Payments from "../pages/admin/Payments";
 
 
-import VendorDashboard from "../pages/vendor/VendorDashboard";
-import VendorSignup from "../pages/vendor/VendorSignup";
-import VendorPO from "../pages/vendor/VendorPO";
-import VendorInvoices from "../pages/vendor/VendorInvoices";
-import Shipment from "../pages/vendor/Shipment";
-import VendorPayments from "../pages/vendor/VendorPayments";
+// import VendorDashboard from "../pages/vendor/VendorDashboard";
+// import VendorSignup from "../pages/vendor/VendorSignup";
+// import VendorPO from "../pages/vendor/VendorPO";
+// import VendorInvoices from "../pages/vendor/VendorInvoices";
+// import Shipment from "../pages/vendor/Shipment";
+// import VendorPayments from "../pages/vendor/VendorPayments";
 
 import DashboardLayout from "../layouts/DashboardLayout";
 
-import AdminDashboard from "../pages/admin/AdminDashboard";
+// import AdminDashboard from "../pages/admin/AdminDashboard";
 
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 
-import VendorApproval from "../pages/admin/VendorApproval"
+// import VendorApproval from "../pages/admin/VendorApproval"
 
 const AppRoutes = () => {
   return (
